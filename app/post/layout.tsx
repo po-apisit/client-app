8987fd66c3d0128cde7535fd1fd0c15a31e9300c
po-apisit <apisit.ton456@gmail.com>
2024-02-12 17:34:26 +0700

    UPDATE

--- app/post/layout.tsx
+++ app/post/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
-import '@fontsource/roboto/700.css';
+import HeaderPost from "./navbar/HeaderPost";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <HeaderPost />
+            {children}
+        </body>
     </html>
   );
 }
---
+++ app/post/navbar/HeaderPost.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+export default function HeaderPost({}: Props) {
+  return (
+    <div>HeaderPost</div>
+  )
+}
